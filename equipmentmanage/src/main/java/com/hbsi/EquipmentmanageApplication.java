e866029a938516788f8cf79075656b7f35514431
qyymm <1035117515@qq.com>
2021-05-31 15:38:21 +0800

    a

---
+++ equipmentmanage/src/main/java/com/hbsi/EquipmentmanageApplication.java
@@ -0,0 +1,13 @@
+package com.hbsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentmanageApplication.class, args);
+    }
+
+}
---
+++ equipmentmanage/src/test/java/com/hbsi/EquipmentmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
