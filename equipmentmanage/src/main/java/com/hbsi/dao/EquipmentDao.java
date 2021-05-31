48935be2baad0842a5e764b6bfd4c2a760bf1c9c
qyymm <1035117515@qq.com>
2021-05-31 17:11:22 +0800

    abc

---
+++ equipmentmanage/src/main/java/com/hbsi/dao/EquipmentDao.java
@@ -0,0 +1,7 @@
+package com.hbsi.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hbsi.entity.Equipment;
+
+public interface EquipmentDao extends BaseMapper<Equipment> {
+}
